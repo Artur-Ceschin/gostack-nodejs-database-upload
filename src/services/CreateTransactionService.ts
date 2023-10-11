bdde1c4993fa78568a2f128bc20daca0fcaa2906
Artur Ceschin <artur.ceschin@gmail.com>
2023-10-11 16:27:39 -0300

    feat: upload config service and trasaction addional logic

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,9 +1,10 @@
-import { getRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
 
 interface Request {
   title: string;
@@ -19,14 +20,45 @@ class CreateTransactionService {
     type,
     category,
   }: Request): Promise<Transaction> {
-    const transactionsRepository = getRepository(TransactionsRepository);
+    try {
+      const transactionsRepository = getCustomRepository(
+        TransactionsRepository,
+      );
+      const categoryRepository = getRepository(Category);
 
-    const createTransaction = await transactionsRepository.create({
-      title,
-      value,
-      type,
-      category,
-    });
+      const { total } = await transactionsRepository.getBalance();
+
+      if (type === 'outcome' && value > total) {
+        throw new AppError('Your outcome is grater than your total value', 400);
+      }
+
+      let transactionCategory = await categoryRepository.findOne({
+        where: {
+          title: category,
+        },
+      });
+
+      if (!transactionCategory) {
+        transactionCategory = categoryRepository.create({
+          title: category,
+        });
+
+        await categoryRepository.save(transactionCategory);
+      }
+
+      const transaction = transactionsRepository.create({
+        title,
+        type,
+        value,
+        category: transactionCategory,
+      });
+
+      await transactionsRepository.save(transaction);
+
+      return transaction;
+    } catch (error) {
+      throw new AppError('An error occurred while creating a transaction', 400);
+    }
   }
 }
 
