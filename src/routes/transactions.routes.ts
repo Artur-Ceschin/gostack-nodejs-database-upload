3fb5d2688227d898ab58b302c984b137a382034a
Artur Ceschin <artur.ceschin@gmail.com>
2023-10-11 16:26:38 -0300

    feat: balance delete and initial upload route added to project

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,14 +1,24 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import multer from 'multer';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import uploadConfig from '../config/upload';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
@@ -16,17 +26,36 @@ transactionsRouter.post('/', async (request, response) => {
 
   const transaction = new CreateTransactionService();
 
-  const newTransaction = transaction.execute({ title, value, type, category });
+  const newTransaction = await transaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
 
   return response.json(newTransaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransaction = new ImportTransactionsService();
+
+    const transactions = await importTransaction.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,28 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    try {
+      const transactionsRepository = getCustomRepository(
+        TransactionsRepository,
+      );
+
+      const transaction = await transactionsRepository.findOne(id);
+
+      if (!transaction) {
+        throw new AppError('Invalid transaction', 400);
+      }
+
+      await transactionsRepository.remove(transaction);
+    } catch (error) {
+      throw new AppError(
+        'Something went wrong while deleting transaction',
+        400,
+      );
+    }
   }
 }
 
